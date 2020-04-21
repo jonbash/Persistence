81e67c46fa3554303e51aafbf6b8921a190d591b
Jon Bash <jonbash@protonmail.com>
2020-04-21 12:39:21 -0700

    remove bad "public"s

--- Sources/Persistence/Persistence.swift
+++ Sources/Persistence/Persistence.swift
@@ -2,12 +2,12 @@ import Foundation
 
 
 public protocol PersistentModel {
-    public static func deleteAll(in context: PersistentContext) throws
+    static func deleteAll(in context: PersistentContext) throws
 }
 
 
-public protocol PersistentFetchable: PersistentModel {
-    public static func fetch(
+public protocol PersistentFetchable: PersistentModel {
+    static func fetch(
         in context: PersistentContext,
         options: FetchOptions?)
         throws -> AnyCollection<Self>
@@ -18,16 +18,16 @@ public protocol PersistentContext {}
 
 
 public protocol PersistentRepository {
-    public func fetch<T: PersistentFetchable>(_ model: T.Type, options: FetchOptions?)
+    func fetch<T: PersistentFetchable>(_ model: T.Type, options: FetchOptions?)
         throws -> AnyCollection<T>
-    public func save<T: PersistentModel>(_ entity: T) throws
-    public func write<T: PersistentModel>(_ entity: T, changes: () throws -> ()) throws
-    public func delete<T: PersistentModel>(_ entity: T) throws
-    public func deleteAll<T: PersistentModel>(_ model: T.Type) throws
+    func save<T: PersistentModel>(_ entity: T) throws
+    func write<T: PersistentModel>(_ entity: T, changes: () throws -> ()) throws
+    func delete<T: PersistentModel>(_ entity: T) throws
+    func deleteAll<T: PersistentModel>(_ model: T.Type) throws
 }
 
 
 public protocol Timestampable {
-    public var dateCreated: Date { get }
-    public var dateModified: Date { get set }
+    var dateCreated: Date { get }
+    var dateModified: Date { get set }
 }
