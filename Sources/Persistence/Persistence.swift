eb3f09defb082360d8e11fcdc0a27135b26a32fb
Jon Bash <jonbash@protonmail.com>
2020-04-21 10:58:46 -0700

    add persistence protocols

--- Sources/Persistence/Persistence.swift
+++ Sources/Persistence/Persistence.swift
@@ -1,3 +1,30 @@
-struct Persistence {
-    var text = "Hello, World!"
+protocol PersistentModel {
+    static func deleteAll(in context: PersistentContext) throws
+}
+
+
+protocol PersistentFetchable: PersistentModel {
+    static func fetch(
+        in context: PersistentContext,
+        options: FetchOptions?)
+        throws -> AnyCollection<Self>
+}
+
+
+protocol PersistentContext {}
+
+
+protocol PersistentRepository {
+    func fetch<T: PersistentFetchable>(_ model: T.Type, options: FetchOptions?)
+        throws -> AnyCollection<T>
+    func save<T: PersistentModel>(_ entity: T) throws
+    func write<T: PersistentModel>(_ entity: T, changes: () throws -> ()) throws
+    func delete<T: PersistentModel>(_ entity: T) throws
+    func deleteAll<T: PersistentModel>(_ model: T.Type) throws
+}
+
+
+protocol Timestampable {
+    var dateCreated: Date { get }
+    var dateModified: Date { get set }
 }
